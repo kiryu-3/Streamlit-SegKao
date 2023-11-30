2a3557d3639cb4f4f534534e30de123aae146e1d
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-12-01 01:48:05 +0900

    Update cist-fass.py

--- Streamlit/CIST-FASS/cist-fass.py
+++ Streamlit/CIST-FASS/cist-fass.py
@@ -5,6 +5,7 @@ import requests
 import io
 from io import BytesIO
 import time
+import pandas as pd
 
 # 画像URLを指定
 image_url = "https://imgur.com/C32lMvR.jpg"
@@ -37,6 +38,7 @@ with st.expander("Human-Flow-Analytics"):
     - CIST's original human flow data visualization tool
     - Uploading data (csv) with latitude and longitude and their time information will allow visualization of the flow of human flow.
     - When the user draws a shape on the map, the number of people who have passed through that shape can be counted.
+    - The CSV file to be uploaded must contain data for ID, time, latitude and longitude that identifies the individual.
   """)
   # st.write("The input text can be converted to speech")
   # st.write("Audio can be downloaded in mp3 format")
@@ -47,7 +49,8 @@ with st.expander("Human-Flow-Analytics"):
 
 with st.expander("CSV-Filter"):
   st.markdown("""
-    - CSVs can be filtered and then downloaded
+    - CSVs can be filtered and then downloaded.
+    - Basic CSV files of all types are supported.
   """)
   # st.write("Can translate input text into a specified language")
   # st.write("The translated text can be downloaded in mp3 format after being converted to audio")
@@ -55,3 +58,18 @@ with st.expander("CSV-Filter"):
   response2 = requests.get("https://imgur.com/kOBBXOb.png")
   image_example2 = Image.open(BytesIO(response2.content))
   st.image(image_example2)
+
+with st.expander("Sample-Data"):
+  st.markdown("""
+    - Sample data with ID, time, latitude and longitude information to identify individuals.
+    - Can be used for "Human-Flow-Analytics" and "CSV-Filter.
+  """)
+
+  example_data = pd.read_csv('example_data.csv')
+  csv_file = example_data.to_csv(index=False)
+  st.download_button(
+    label="Download CSV",
+    data=csv_file,
+    file_name='sample_data.csv'
+  )
+  st.dataframe(example_data)
