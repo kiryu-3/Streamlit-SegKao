9a87cdb474612bcd5c1e7e802d8b64b37788b23a
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-12-27 02:23:18 +0900

    Update regression.py

--- Streamlit/EDAML-hub/pages/regression.py
+++ Streamlit/EDAML-hub/pages/regression.py
@@ -7,17 +7,69 @@ import matplotlib.figure as figure
 # ML
 from pycaret.regression import *
 
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    return df
+
+def upload_csv():
+    # csvがアップロードされたとき
+    if st.session_state['upload_csvfile'] is not None:
+        if 'df' not in st.session_state:
+            # アップロードされたファイルデータを読み込む
+            file_data = st.session_state['upload_csvfile'].read()
+            # バイナリデータからPandas DataFrameを作成
+            try:
+                df = pd.read_csv(io.BytesIO(file_data), encoding="utf-8", engine="python")
+                st.session_state["ja_honyaku"] = False
+            except UnicodeDecodeError:
+                # UTF-8で読み取れない場合はShift-JISエンコーディングで再試行
+                df = pd.read_csv(io.BytesIO(file_data), encoding="shift-jis", engine="python")
+                st.session_state["ja_honyaku"] = True
+    
+            # カラムの型を自動で適切に変換
+            st.session_state['df'] = reduce_mem_usage(df)
+        else:
+            pass
 
 def load_and_explore_data():
-    uploaded_file = st.file_uploader('データを読み込んで下さい。', type=['csv'])
+    st.file_uploader("CSVファイルをアップロード",
+                       type=["csv"],
+                       key="upload_csvfile",
+                       on_change=upload_csv
+                       )
 
-    if uploaded_file:
-        # キャッシュからデータを取得し、存在しない場合は新たにデータをロードしてキャッシュに保存
-        if "data_cache" in st.session_state:
-            data = st.session_state.data_cache
-        else:
-            data = pd.read_csv(uploaded_file)
-            st.session_state.data_cache = data
+    # if uploaded_file:
+    #     # キャッシュからデータを取得し、存在しない場合は新たにデータをロードしてキャッシュに保存
+    #     if "data_cache" in st.session_state:
+    #         data = st.session_state.data_cache
+    #     else:
+    #         data = pd.read_csv(uploaded_file)
+    #         st.session_state.data_cache = data
+
+        data = st.session_state['df'].copy()
 
         st.write('データの確認')
         st.write(data)
