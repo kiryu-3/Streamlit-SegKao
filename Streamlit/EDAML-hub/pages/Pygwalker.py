855d252368ecf08e5f1682df36be0a78464f0d43
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-12-27 02:29:58 +0900

    Update Pygwalker.py

--- Streamlit/EDAML-hub/pages/Pygwalker.py
+++ Streamlit/EDAML-hub/pages/Pygwalker.py
@@ -5,8 +5,34 @@ import streamlit.components.v1 as components
 import pygwalker as pyg
 from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
 
-# 初期設定
-st.set_page_config(layout="wide")
+import re
+import requests
+from PIL import Image
+import io
+from io import BytesIO
+
+# 画像URLを指定
+image_url = "https://imgur.com/C32lMvR.jpg"
+
+# 画像をダウンロードしPILのImageオブジェクトとして読み込む
+response = requests.get(image_url)
+image = Image.open(BytesIO(response.content))
+
+# Streamlit ページの設定
+st.set_page_config(
+    page_title="EDAML-hub",
+    page_icon=image,
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 
 init_streamlit_comm()
 
