3b0785e57461d27c365789dd0a875b7c50726ba3
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-12-26 22:13:59 +0900

    Update CSV_Filter.py

--- Streamlit/CIST-FASS/pages/CSV_Filter.py
+++ Streamlit/CIST-FASS/pages/CSV_Filter.py
@@ -146,6 +146,10 @@ if st.session_state["upload_csvfile"] is not None:
                      options=st.session_state["uploaded_df"].columns,
                      key="selected_columns",
                      on_change=select_column)
+    # カラムとデータ型の一覧を表示
+    column_info = st.session_state["all_df"].dtypes.reset_index()
+    column_info.columns = ['Column', 'Data Type']
+    tab2.write(column_info)
     # tab2.header("")
 
     upload_name = st.session_state['upload_csvfile'].name
