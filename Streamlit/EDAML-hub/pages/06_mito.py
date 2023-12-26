5cc3ce8b7da189f55508a218c6f5ea329ecd4946
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-12-27 03:51:14 +0900

    Create 06_mito.py

---
+++ Streamlit/EDAML-hub/pages/06_mito.py
@@ -0,0 +1,113 @@
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from mitosheet.streamlit.v1 import spreadsheet
+import re
+import requests
+from PIL import Image
+import io
+from io import BytesIO
+
+# 画像URLを指定
+image_url = "https://imgur.com/C32lMvR.jpg"
+
+# 画像をダウンロードしPILのImageオブジェクトとして読み込む
+response = requests.get(image_url)
+image = Image.open(BytesIO(response.content))
+
+# Streamlit ページの設定
+st.set_page_config(
+    page_title="EDAML-hub",
+    page_icon=image,
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    return df
+
+def upload_csv():
+    # csvがアップロードされたとき
+    if st.session_state['upload_csvfile'] is not None:
+        # アップロードされたファイルデータを読み込む
+        file_data = st.session_state['upload_csvfile'].read()
+        # バイナリデータからPandas DataFrameを作成
+        try:
+            df = pd.read_csv(io.BytesIO(file_data), encoding="utf-8", engine="python")
+            st.session_state["ja_honyaku"] = False
+        except UnicodeDecodeError:
+            # UTF-8で読み取れない場合はShift-JISエンコーディングで再試行
+            df = pd.read_csv(io.BytesIO(file_data), encoding="shift-jis", engine="python")
+            st.session_state["ja_honyaku"] = True
+
+        # カラムの型を自動で適切に変換
+        st.session_state['df'] = reduce_mem_usage(df)
+
+st.title('Pygwalker')
+st.file_uploader("CSVファイルをアップロード",
+                       type=["csv"],
+                       key="upload_csvfile",
+                       on_change=upload_csv
+                       )
+
+# Graphic Walker 操作（メインパネル）
+if st.session_state['upload_csvfile'] is not None:
+    final_dfs, code = spreadsheet(st.session_state['df'])
+
+    with st.expander("data"):
+      st.dataframe(final_dfs)
+  
+    with st.expander("code"):
+      st.code(code)
+
+    upload_name = st.session_state['upload_csvfile'].name
+    download_name = upload_name.split(".")[0]
+    st.write("ファイル名を入力してください")
+    st.text_input(
+        label="Press Enter to Apply",
+        value=f"{download_name}_filtered",
+        key="download_name"
+    )
+  
+    if st.session_state["ja_honyaku"]:
+        csv_file = download_df.to_csv(index=False, encoding="shift-jis")
+    else:
+        csv_file = download_df.to_csv(index=False, encoding="utf-8")
+    st.download_button(
+        label="Download CSV",
+        data=csv_file,
+        file_name=f'{st.session_state["download_name"]}.csv'
+    )
