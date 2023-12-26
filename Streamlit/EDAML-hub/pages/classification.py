68d4ba2ca79ec58b05b8ed2c3ed91606e4439b60
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-12-27 02:28:17 +0900

    Update classification.py

--- Streamlit/EDAML-hub/pages/classification.py
+++ Streamlit/EDAML-hub/pages/classification.py
@@ -8,6 +8,34 @@ from sklearn import metrics
 # ML
 from pycaret.classification import *
 
+import re
+import requests
+from PIL import Image
+import io
+from io import BytesIO
+
+# 画像URLを指定
+image_url = "https://imgur.com/C32lMvR.jpg"
+
+# 画像をダウンロードしPILのImageオブジェクトとして読み込む
+response = requests.get(image_url)
+image = Image.open(BytesIO(response.content))
+
+# Streamlit ページの設定
+st.set_page_config(
+    page_title="EDAML-hub",
+    page_icon=image,
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 @st.cache_data()
 def load_data(uploaded_file):
     return pd.read_csv(uploaded_file)
