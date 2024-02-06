bc078915925857ec71697e0300be0f924080e076
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-02-07 02:15:49 +0900

    Update 06_mito.py

--- Streamlit/EDAML-hub/pages/06_mito.py
+++ Streamlit/EDAML-hub/pages/06_mito.py
@@ -123,95 +123,46 @@ else:
                            on_change=upload_xlsx
                            )
 
-# Graphic Walker 操作（メインパネル）
-if len(st.session_state['df']) != 0:
-    final_dfs, code = spreadsheet(*st.session_state['df'])
-
-    tabs_list = list()
-    for idx, (key, value) in enumerate(final_dfs.items()):
-        tabs_list.append(f"df_{idx+1}")
-    # タブ
-    tabs = st.tabs(tabs_list)
-
-    for idx, (key, value) in enumerate(final_dfs.items()):
-        with tabs[idx]:
-            st.caption(f"df_{idx+1}")
-            st.dataframe(pd.DataFrame(value))
-            dfdf = pd.DataFrame(value)
-            container = st.beta_container()
-            for i in range(0, len(dfdf), 50):
-                container.dataframe(dfdf[i:i+50])
-
-            download_name = f"df_{idx+1}"
-            st.write("ファイル名を入力してください")
-            st.text_input(
-              label="Press Enter to Apply",
-              value=f"{download_name}_filtered",
-              key=f"download_name_{idx}"
-            )
-
-            download_df = pd.DataFrame(value)
-            if st.session_state['select_mode'] == "***CSVファイル***":
-                if st.session_state["ja_honyaku"][idx]:
-                  csv_file = download_df.to_csv(index=False, encoding="shift-jis")
-                else:
-                  csv_file = download_df.to_csv(index=False, encoding="utf-8")    
-            else:
-                csv_file = download_df.to_csv(index=False)
-            st.download_button(
-                  label="Download CSV",
-                  data=csv_file,
-                  file_name=f'{st.session_state[f"download_name_{idx}"]}.csv'
+try: 
+    if len(st.session_state['df']) != 0:
+        final_dfs, code = spreadsheet(*st.session_state['df'])
+    
+        tabs_list = list()
+        for idx, (key, value) in enumerate(final_dfs.items()):
+            tabs_list.append(f"df_{idx+1}")
+        # タブ
+        tabs = st.tabs(tabs_list)
+    
+        for idx, (key, value) in enumerate(final_dfs.items()):
+            with tabs[idx]:
+                st.caption(f"df_{idx+1}")
+                st.dataframe(pd.DataFrame(value))
+    
+                download_name = f"df_{idx+1}"
+                st.write("ファイル名を入力してください")
+                st.text_input(
+                  label="Press Enter to Apply",
+                  value=f"{download_name}_filtered",
+                  key=f"download_name_{idx}"
                 )
-            
-    st.divider()
     
-    with st.expander("Code"):
-        st.code(code)
-# try: 
-#     if len(st.session_state['df']) != 0:
-#         final_dfs, code = spreadsheet(*st.session_state['df'])
-    
-#         tabs_list = list()
-#         for idx, (key, value) in enumerate(final_dfs.items()):
-#             tabs_list.append(f"df_{idx+1}")
-#         # タブ
-#         tabs = st.tabs(tabs_list)
-    
-#         for idx, (key, value) in enumerate(final_dfs.items()):
-#             with tabs[idx]:
-#                 st.caption(f"df_{idx+1}")
-#                 st.dataframe(pd.DataFrame(value))
-#                 dfdf = pd.DataFrame(value)
-#                 container = st.beta_container()
-#                 for i in range(0, len(dfdf), 50):
-#                     container.dataframe(dfdf[i:i+50])
-    
-#                 download_name = f"df_{idx+1}"
-#                 st.write("ファイル名を入力してください")
-#                 st.text_input(
-#                   label="Press Enter to Apply",
-#                   value=f"{download_name}_filtered",
-#                   key=f"download_name_{idx}"
-#                 )
-    
-#                 download_df = pd.DataFrame(value)
-#                 if st.session_state['select_mode'] == "***CSVファイル***":
-#                     if st.session_state["ja_honyaku"][idx]:
-#                       csv_file = download_df.to_csv(index=False, encoding="shift-jis")
-#                     else:
-#                       csv_file = download_df.to_csv(index=False, encoding="utf-8")    
-#                 else:
-#                     csv_file = download_df.to_csv(index=False)
-#                 st.download_button(
-#                       label="Download CSV",
-#                       data=csv_file,
-#                       file_name=f'{st.session_state[f"download_name_{idx}"]}.csv'
-#                     )
+                download_df = pd.DataFrame(value)
+                if st.session_state['select_mode'] == "***CSVファイル***":
+                    if st.session_state["ja_honyaku"][idx]:
+                      csv_file = download_df.to_csv(index=False, encoding="shift-jis")
+                    else:
+                      csv_file = download_df.to_csv(index=False, encoding="utf-8")    
+                else:
+                    csv_file = download_df.to_csv(index=False)
+                st.download_button(
+                      label="Download CSV",
+                      data=csv_file,
+                      file_name=f'{st.session_state[f"download_name_{idx}"]}.csv'
+                    )
                 
-#         st.divider()
+        st.divider()
         
-#         with st.expander("Code"):
-#             st.code(code)
-# except:
-#     pass
+        with st.expander("Code"):
+            st.code(code)
+except:
+    pass
