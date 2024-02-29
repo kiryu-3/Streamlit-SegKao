6999942a58d3d09101faec3544a883ec22c9b57d
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-02-29 10:56:26 +0900

    Update Human-Flow-Analytics.py

--- Streamlit/CIST-FASS/pages/Human-Flow-Analytics.py
+++ Streamlit/CIST-FASS/pages/Human-Flow-Analytics.py
@@ -22,18 +22,11 @@ import io
 from io import BytesIO
 import itertools
 
-# 画像URLを指定
-image_url = "https://github.com/kiryu-3/Streamlit-SegKao/blob/main/Streamlit/CIST-FASS/icon_image.png"
+# 画像ファイルのパス
+image_path = os.path.join(os.path.dirname(__file__), '..', 'icon_image.png')
 
-# 画像をダウンロードしPILのImageオブジェクトとして読み込む
-response = requests.get(image_url)
-image = Image.open(BytesIO(response.content))
-
-# # 画像ファイルのパス
-# image_path = os.path.join(os.path.dirname(__file__), '..', '..', 'icon_image.png')
-
-# # 画像をPILのImageオブジェクトとして読み込む
-# image = Image.open(image_path)
+# 画像をPILのImageオブジェクトとして読み込む
+image = Image.open(image_path)
 
 # Streamlit ページの設定
 st.set_page_config(
