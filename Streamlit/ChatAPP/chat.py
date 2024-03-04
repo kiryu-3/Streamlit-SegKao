5bef69c05f258a942679c353edc429d8ff8e4a13
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-03-05 01:37:58 +0900

    Create chat.py

---
+++ Streamlit/ChatAPP/chat.py
@@ -0,0 +1,112 @@
+import time 
+import sqlite3 
+
+import bcrypt
+from PIL import Image
+import pandas as pd
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+from datetime import datetime, timedelta
+
+def setup_database():
+    conn = sqlite3.connect('chat.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS chats
+                 (date TEXT, group_name TEXT, username TEXT, comment TEXT)''')
+    conn.commit()
+    return conn, c
+
+def select_group_and_username():
+    today = datetime.today()  # 今日の日付を取得 
+    lastday = today - timedelta(days=365)  # 一年前の日付を計算
+    # 日付を選択する入力ウィジェットを表示し、ユーザーが日付を選択する
+    date = st.sidebar.date_input(
+        label="日付を選択してください",
+        value=today,  # 初期値を今日の日付に設定
+        min_value=lastday,  # 一年前の日付を最小値に設定
+        max_value=today  # 今日の日付を最大値に設定
+    )
+
+    group = st.sidebar.selectbox('グループを選択してください', ['グループA', 'グループB', 'グループC', 'グループD', 'グループE', 'グループF', 'グループG'])
+    username = st.sidebar.text_input('ユーザー名を入力してください')
+
+    st.session_state["date"] = date
+    st.session_state["group"] = group
+    st.session_state["username"] = username
+    
+
+    st.sidebar.write(f'date：{st.session_state["date"]}')
+    st.sidebar.write(f'group：{st.session_state["group"]}')
+    st.sidebar.write(f'username：{st.session_state["username"]}')
+    
+    return date, group, username
+
+def save_comment_to_database(conn, c, date, group, username, comment):
+    if comment:
+        c.execute("INSERT INTO chats (date, group_name, username, comment) VALUES (?, ?, ?, ?)", (date, group, username, comment))
+        conn.commit()
+
+def display_chat_input(c, date, group):
+    chat_rows = c.execute("SELECT username, comment FROM chats WHERE group_name=? AND date=?", (group, date))
+
+    for row in chat_rows:
+        if row[0] == st.session_state["username"]:
+            st.chat_message("user").write(f"{row[0]}:\n {row[1]}")
+        else:
+            st.chat_message("assistant").write(f"{row[0]}:\n {row[1]}")
+
+def load_credentials(filepath):
+    with open(filepath, 'r') as file:
+        data = yaml.safe_load(file)
+
+    names = [data['credentials']['usernames'][key]['name'] for key in data['credentials']['usernames']]
+    usernames = list(data['credentials']['usernames'].keys())
+    passwords = [data['credentials']['usernames'][key]['password'] for key in data['credentials']['usernames']]
+    return names, usernames, passwords, data['cookie']['name'], data['cookie']['key'], data['cookie']['expiry_days']
+
+def authenticate(names, usernames, passwords, cookie_name, key, expiry_days):
+    authenticator = stauth.Authenticate(
+        names=names,
+        usernames=usernames,
+        passwords=passwords,
+        cookie_name=cookie_name,
+        key=key,
+        cookie_expiry_days=expiry_days,
+    )
+    return authenticator
+
+def login(authenticator):
+    name, authentication_status, username = authenticator.login('Login', 'sidebar')
+    st.session_state.name = name
+    st.session_state.authentication_status = authentication_status
+    st.session_state.username = username
+    return name, authentication_status, username
+
+def logout():
+    if st.sidebar.button("Logout"):
+        st.session_state['authentication_status'] = None
+        st.rerun()
+
+def process_authentication(authentication_status):
+    if authentication_status:
+        conn, c = setup_database()
+        date, group, username = select_group_and_username()
+
+        if len(username) != 0:
+            comment = st.text_area('コメントを入力してください')
+            if st.button('送信'):
+                save_comment_to_database(conn, c, date, group, username, comment)
+            display_chat_input(c, date, group)
+    elif authentication_status == False:
+        st.sidebar.error('Username/password is incorrect')
+    elif authentication_status == None:
+        st.sidebar.warning('Please enter your username and password')
+
+names, usernames, passwords, cookie_name, key, expiry_days = load_credentials('config.yaml')
+authenticator = authenticate(names, usernames, passwords, cookie_name, key, expiry_days)
+name, authentication_status, username = login(authenticator)
+if authentication_status:
+    logout()
+process_authentication(authentication_status)
