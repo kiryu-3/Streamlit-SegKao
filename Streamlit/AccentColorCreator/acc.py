864001ce66bb03cfeaa5b087ce67ee416dfed2aa
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-03-25 00:02:36 +0900

    Update acc.py

--- Streamlit/AccentColorCreator/acc.py
+++ Streamlit/AccentColorCreator/acc.py
@@ -59,7 +59,7 @@ with col1:
 
 with col2:
     accent_color = rotate_hue(main_color, 120)
-    st.color_picker('your accent color', accent_color, disabled=True)   
+    st.color_picker('your accent color', value=accent_color, disabled=True)   
     st.write('Your accent color:', accent_color)
 
     accent_rgb_color = hex_to_rgb(accent_color)
