52f1c0d03b26fc437c9baa386752a429e1df955c
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-03-31 00:34:55 +0900

    Update shape_manager.py

--- Streamlit/cist-fass/utils/shape_manager.py
+++ Streamlit/cist-fass/utils/shape_manager.py
@@ -2,7 +2,7 @@ class ShapeManager:
     def __init__(self, map_manager):
         self.map_manager = map_manager
 
-    def handle_draw_data(self, draw_data):
+    def handle_draw_data(self, data_manager, draw_data):
         self.map_manager.add_draw_data(draw_data)
 
     def select_shape(self, shape_id):
