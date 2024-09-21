992b6c0785dd3845004cb5ea5da0ed2eb6d1b53d
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-09-21 17:12:13 +0900

    Update top.py

--- Streamlit/InusAnalysis/top.py
+++ Streamlit/InusAnalysis/top.py
@@ -5,7 +5,7 @@ import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import japanize_matplotlib
+import matplotlib_fontja
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
