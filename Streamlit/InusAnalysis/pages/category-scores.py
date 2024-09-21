d275175d8c9e3a30414cbceb6851de8947f3ee4a
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-09-21 18:13:02 +0900

    Update category-scores.py

--- Streamlit/InusAnalysis/pages/category-scores.py
+++ Streamlit/InusAnalysis/pages/category-scores.py
@@ -239,7 +239,7 @@ st.file_uploader("CSVファイルをアップロード",
 # データフレームを空にするボタン
 if st.button("アップロードしたCSVファイルを消去"):
     st.session_state['df'] = pd.DataFrame()  # 空のデータフレームを設定
-    st.session_state["page_control"] = 0
+    st.switch_page("top.py")
     st.rerun()  # アプリを再実行して状態を更新
     st.success("CSVファイルが消去されました。")
 
