5d21d1dce87e7287d4c03a70ba4179206a9cf9f3
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-09-21 17:07:47 +0900

    Update required-seconds.py

--- Streamlit/InusAnalysis/pages/required-seconds.py
+++ Streamlit/InusAnalysis/pages/required-seconds.py
@@ -5,6 +5,7 @@ import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import japanize_matplotlib
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
