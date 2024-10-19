5227a4ee13905a8ba123235c9f528ad60d230c7e
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-10-19 18:51:53 +0900

    Update required-seconds.py

--- Streamlit/InusAnalysis/pages/required-seconds.py
+++ Streamlit/InusAnalysis/pages/required-seconds.py
@@ -228,7 +228,7 @@ try:
 
     # 表形式で表示
     cols = st.columns(2)
-    cols[0].write("### 各学年の人数")
+    cols[0].write("####  各学年の人数")
     cols[0].dataframe(summary_df)
     cols[1].write("### 各分野の質問数")
     cols[1].dataframe(question_df)
