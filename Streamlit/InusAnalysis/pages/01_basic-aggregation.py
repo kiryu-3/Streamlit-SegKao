2ba410199bea8f99f57ca75034ebedf6992832f0
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-10-20 19:24:57 +0900

    Update 01_basic-aggregation.py

--- Streamlit/InusAnalysis/pages/01_basic-aggregation.py
+++ Streamlit/InusAnalysis/pages/01_basic-aggregation.py
@@ -271,7 +271,7 @@ def analyze_selected_category(selected_category, grades, df, question_df):
             else:
                 st.write("学年間のスコアの有意差はありません")
     
-st.header("設問ごとの分析")    
+st.header("設問別スコアの分析")    
 # ファイルアップロード
 st.file_uploader("集計結果（5件法）のcsvをアップロード",
                        type=["csv"],
