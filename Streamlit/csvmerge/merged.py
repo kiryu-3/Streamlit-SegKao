9889fd55f73f003cee0621dc600336d1a111a831
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-10-30 03:15:18 +0900

    Update merged.py

--- Streamlit/csvmerge/merged.py
+++ Streamlit/csvmerge/merged.py
@@ -158,6 +158,7 @@ try:
         # st.write(merged_df.columns)
         
         csv_file = merged_df.to_csv(index=False, quotechar='"', encoding=st.session_state['encoding'])
+        st.write(st.session_state['encoding'])
         st.download_button(
                   label="Download CSV",
                   data=csv_file,
