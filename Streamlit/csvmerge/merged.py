39eaecfb4bf076962375d0a2b2ad23c6f6dfbac1
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-10-30 00:02:14 +0900

    Update merged.py

--- Streamlit/csvmerge/merged.py
+++ Streamlit/csvmerge/merged.py
@@ -64,7 +64,7 @@ def upload_csv():
         st.write(encoding)
         
         try:
-            df = pd.read_csv(io.BytesIO(file_data), header=None, encoding=encoding, engine="python")
+            df = pd.read_csv(io.BytesIO(file_data), header=None, encoding=encoding, error_bad_lines=False, engine="python")
         except Exception as e:
             st.write(f"データの読み込み中にエラーが発生しました: {e}")
 
