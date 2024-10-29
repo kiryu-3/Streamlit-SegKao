6b6620e99f595d60e6743946714285764429bf5a
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-10-29 16:22:09 +0900

    Update merged.py

--- Streamlit/csvmerge/merged.py
+++ Streamlit/csvmerge/merged.py
@@ -101,27 +101,27 @@ try:
             st.write(df)
         
     
-                download_name = f"df_{idx+1}"
-                st.write("ファイル名を入力してください")
-                st.text_input(
-                  label="Press Enter to Apply",
-                  value=f"{download_name}_filtered",
-                  key=f"download_name_{idx}"
-                )
-    
-                download_df = pd.DataFrame(value)
-                if st.session_state['select_mode'] == "***CSVファイル***":
-                    if st.session_state["ja_honyaku"][idx]:
-                      csv_file = download_df.to_csv(index=False, encoding="shift-jis")
-                    else:
-                      csv_file = download_df.to_csv(index=False, encoding="utf-8")    
+            download_name = f"df_{idx+1}"
+            st.write("ファイル名を入力してください")
+            st.text_input(
+              label="Press Enter to Apply",
+              value=f"{download_name}_filtered",
+              key=f"download_name_{idx}"
+            )
+
+            download_df = pd.DataFrame(value)
+            if st.session_state['select_mode'] == "***CSVファイル***":
+                if st.session_state["ja_honyaku"][idx]:
+                  csv_file = download_df.to_csv(index=False, encoding="shift-jis")
                 else:
-                    csv_file = download_df.to_csv(index=False)
-                st.download_button(
-                      label="Download CSV",
-                      data=csv_file,
-                      file_name=f'{st.session_state[f"download_name_{idx}"]}.csv'
-                    )
+                  csv_file = download_df.to_csv(index=False, encoding="utf-8")    
+            else:
+                csv_file = download_df.to_csv(index=False)
+            st.download_button(
+                  label="Download CSV",
+                  data=csv_file,
+                  file_name=f'{st.session_state[f"download_name_{idx}"]}.csv'
+                )
                 
         st.divider()
         
