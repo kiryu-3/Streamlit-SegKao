adff46488c98bdbc0221181ad9fc86f9ce4e94cd
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-10-30 02:44:32 +0900

    Update merged.py

--- Streamlit/csvmerge/merged.py
+++ Streamlit/csvmerge/merged.py
@@ -66,6 +66,7 @@ def upload_csv():
         st.session_state['df'] = df
     else:
         st.session_state['df'] = pd.DataFrame()
+        st.session_state['question_df'] = dict()
 
 def upload_csv2():
     # csvがアップロードされたとき
@@ -96,7 +97,6 @@ def upload_csv2():
                 (f"データの読み込み中にエラーが発生しました: {e}")
 
     else:
-        st.session_state['df'] = pd.DataFrame()
         st.session_state['question_df'] = dict()
 
 # 初期化
