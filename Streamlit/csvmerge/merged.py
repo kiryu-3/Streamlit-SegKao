01a0b4d73bc5957d926fa24a7c468c38c44f9b2b
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-10-30 19:12:18 +0900

    Update merged.py

--- Streamlit/csvmerge/merged.py
+++ Streamlit/csvmerge/merged.py
@@ -207,9 +207,10 @@ try:
                 
 
                 for q_candidate in q_candidates:
-                   st.write(q_candidate) 
+                   
                    # 各 unique_value を正規化し、比較対象も正規化
                    if not any(q_candidate in value for value in unique_values):
+                        st.write(q_candidate) 
                         q_sentence.append(q_candidate)
 
             
