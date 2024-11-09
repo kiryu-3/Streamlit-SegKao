867361db194630ccdec93734b75541eea8ad4dbd
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-11-10 05:04:11 +0900

    Update kansei.py

--- Streamlit/csvkakou/kansei.py
+++ Streamlit/csvkakou/kansei.py
@@ -3,9 +3,13 @@ import streamlit as st
 def page_2():
     st.title("Page 2")
 
+def page_3():
+    st.title("Page 3")
+
 pages = {
     "Your account": [
         st.Page(page_2),
+        st.Page(page_3),
         st.Page("prokiso.py", title="gg"),
     ]
 }
