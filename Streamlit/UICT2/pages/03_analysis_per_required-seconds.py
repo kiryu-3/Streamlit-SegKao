495fd1bab5839643bcde46fe12b0e1beb919dafa
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-12-09 04:07:47 +0900

    Update 03_analysis_per_required-seconds.py

--- Streamlit/UICT2/pages/03_analysis_per_required-seconds.py
+++ Streamlit/UICT2/pages/03_analysis_per_required-seconds.py
@@ -132,7 +132,7 @@ def normality_test(df, categories):
 
     # 正規性の検証
     results = {}
-    stat, p = stats.shapiro(df["required_time_seconds"])
+    stat, p = stats.shapiro(df["required-seconds"])
     results["required-seconds"] = {
         'W統計量': stat,
         'p値': p,
