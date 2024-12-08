1d2f15e255206f0ec318ae1859cd941a36e3c69d
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2024-12-09 04:27:09 +0900

    Update 04_analysis_per_comment.py

--- Streamlit/UICT2/pages/04_analysis_per_comment.py
+++ Streamlit/UICT2/pages/04_analysis_per_comment.py
@@ -464,7 +464,7 @@ if option == "回答形式":
     # qcategory が option に等しい行を取り出す
     questionnaires_df = st.session_state['questionnaires_df'][st.session_state['questionnaires_df']['qcategory'] == option]
     st.write(questionnaires_df)
-    st.write(questionnaires_df.at[0, 0])
+    st.write(questionnaires_df.at[0, "qsentence"])
     analyze_selected_category(option, grades, sorted_df.iloc[:, [0, 1]], questionnaires_df.at[0, "qsentence"])
 
     st.write(questionnaires_df.at[1, "qsentence"])
